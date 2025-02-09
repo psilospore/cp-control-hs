dc1988805a768d1cc02c345bf2fb54dfbd79a9a2
Ravi Mangal <ravi9788@gmail.com>
2025-02-09 15:13:04 -0700

    (1) Add some system models; (2) Add instructions to setup and run Tempest

---
+++ system_models/taxinet_Boeing5bins_perfect_shielding.pm
@@ -0,0 +1,258 @@
+mdp
+
+const N=5;
+
+const double prob=1/15;
+
+const double prob_success=0.95;
+const double prob_fail=(1-prob_success)/2;
+
+module taxinet
+
+	//  state
+	cte : [-1..4] init 0;
+	// cte=0: on centerline
+	// cte=1: left of centerline
+	// cte=2: right of centerline
+	// cte=3: more left
+ 	// cte=4: more right
+	// cte=-1: off runway ERROR STATE
+
+	he: [-1..2] init 0;
+	// he=0: no angle
+	// he=1: negative
+	// he=2: positive
+	// he=-1: ERROR
+
+	// encoding result of run-time guard
+	v: [0..1] init 1;
+	// v=1: certified
+	// v=0: not certified
+
+	// estimate of the state from NN
+	cte_est: [0..4] init 0;
+	he_est: [0..2] init 0; 
+	
+
+  	// program counter
+	pc:[-1..5] init -1; 
+	// 5 is now "sink state"
+
+	// steps
+	k:[1..N] init 1;
+
+	// actions issued by controller
+	a:[-1..2] init -1;
+	// a=-1: still to decide 
+	// a=0: go straight
+	// a=1: turn left
+	// a=2: turn right
+
+	// Randomise initial position
+	[initialise] pc=-1 -> 
+		prob: (cte'=0)&(he'=0)&(pc'=0)
+		+ prob: (cte'=0)&(he'=1)&(pc'=0)
+		+ prob: (cte'=0)&(he'=2)&(pc'=0)
+
+		+ prob: (cte'=1)&(he'=0)&(pc'=0)
+		+ prob: (cte'=1)&(he'=1)&(pc'=0)
+		+ prob: (cte'=1)&(he'=2)&(pc'=0)
+
+		+ prob: (cte'=2)&(he'=0)&(pc'=0)
+		+ prob: (cte'=2)&(he'=1)&(pc'=0)
+		+ prob: (cte'=2)&(he'=2)&(pc'=0)
+
+		+ prob: (cte'=3)&(he'=0)&(pc'=0)
+		+ prob: (cte'=3)&(he'=1)&(pc'=0)
+		+ prob: (cte'=3)&(he'=2)&(pc'=0)
+
+		+ prob: (cte'=4)&(he'=0)&(pc'=0)
+		+ prob: (cte'=4)&(he'=1)&(pc'=0)
+		+ prob: (cte'=4)&(he'=2)&(pc'=0);
+
+
+	// run-time guard: assume we can certify all inputs	
+	[]  pc=0 -> 1: (v'=1) & (pc'=1);	
+
+	// NN perfect perception 
+	[] cte=0 & v=1 & pc=1 -> 1.0: (cte_est'=0) & (pc'=2) + 
+				 0.0: (cte_est'=1) & (pc'=2) + 
+				 0.0: (cte_est'=2) & (pc'=2) + 
+				 0.0: (cte_est'=3) & (pc'=2) + 
+				 0.0: (cte_est'=4) & (pc'=2);
+	[] cte=1 & v=1 & pc=1 -> 0.0: (cte_est'=0) & (pc'=2) + 
+				 1.0: (cte_est'=1) & (pc'=2) + 
+				 0.0: (cte_est'=2) & (pc'=2) + 
+				 0.0: (cte_est'=3) & (pc'=2) + 
+				 0.0: (cte_est'=4) & (pc'=2);
+	[] cte=2 & v=1 & pc=1 -> 0.0: (cte_est'=0) & (pc'=2) + 
+				 0.0: (cte_est'=1) & (pc'=2) + 
+				 1.0: (cte_est'=2) & (pc'=2) + 
+				 0.0: (cte_est'=3) & (pc'=2) + 
+				 0.0: (cte_est'=4) & (pc'=2);
+	[] cte=3 & v=1 & pc=1 -> 0.0: (cte_est'=0) & (pc'=2) + 
+				 0.0: (cte_est'=1) & (pc'=2) + 
+				 0.0: (cte_est'=2) & (pc'=2) + 
+				 1.0: (cte_est'=3) & (pc'=2) + 
+				 0.0: (cte_est'=4) & (pc'=2);
+	[] cte=4 & v=1 & pc=1 -> 0.0: (cte_est'=0) & (pc'=2) + 
+				 0.0: (cte_est'=1) & (pc'=2) + 
+				 0.0: (cte_est'=2) & (pc'=2) + 
+				 0.0: (cte_est'=3) & (pc'=2) + 
+				 1.0: (cte_est'=4) & (pc'=2);
+
+
+	[] he=0 & v=1 & pc=2 -> 1.0: (he_est'=0) & (pc'=3) + 0.0: (he_est'=1) & (pc'=3) + 0.0: (he_est'=2) & (pc'=3); 	
+	[] he=1 & v=1 & pc=2 -> 0.0: (he_est'=0) & (pc'=3) + 1.0: (he_est'=1) & (pc'=3) + 0.0: (he_est'=2) & (pc'=3);
+	[] he=2 & v=1 & pc=2 -> 0.0: (he_est'=0) & (pc'=3) + 0.0: (he_est'=1) & (pc'=3) + 1.0: (he_est'=2) & (pc'=3);
+
+
+	// Select actions
+	//prob_success:(a'=)&pc'=4 + 0.0prob_fail'=)&(pc'=4) + 0.05prob_fail)&(pc'=4);
+
+	/// CTE=0 ///
+	[straight] cte_est=0 & he_est=0 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] cte_est=0 & he_est=0 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] cte_est=0 & he_est=0 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+	[straight] cte_est=0 & he_est=1 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] cte_est=0 & he_est=1 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] cte_est=0 & he_est=1 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+	[straight] cte_est=0 & he_est=2 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] cte_est=0 & he_est=2 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] cte_est=0 & he_est=2 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+	
+
+	/// CTE=1|3 ///
+	[straight] (cte_est=1 | cte_est=3) & he_est=0 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=1 | cte_est=3) & he_est=0 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=1 | cte_est=3) & he_est=0 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+	[straight] (cte_est=1 | cte_est=3) & he_est=1 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=1 | cte_est=3) & he_est=1 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=1 | cte_est=3) & he_est=1 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+	
+	[straight] (cte_est=1 | cte_est=3) & he_est=2 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=1 | cte_est=3) & he_est=2 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=1 | cte_est=3) & he_est=2 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+	
+
+	/// CTE=2|4 ///
+	[straight] (cte_est=2 | cte_est=4) & he_est=0 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=2 | cte_est=4) & he_est=0 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=2 | cte_est=4) & he_est=0 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+
+	[straight] (cte_est=2 | cte_est=4) & he_est=1 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=2 | cte_est=4) & he_est=1 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=2 | cte_est=4) & he_est=1 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+
+	[straight] (cte_est=2 | cte_est=4) & he_est=2 & pc=3 -> 
+		prob_success:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[left] (cte_est=2 | cte_est=4) & he_est=2 & pc=3 -> 
+		prob_success:(a'=1)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=2)&(pc'=4);
+	[right] (cte_est=2 | cte_est=4) & he_est=2 & pc=3 -> 
+		prob_success:(a'=2)&(pc'=4) + prob_fail:(a'=0)&(pc'=4) + prob_fail:(a'=1)&(pc'=4);
+
+	
+	//[straight] cte_est=0 & he_est=0 & pc=3 -> 1 : (a'=a_0_0) & (pc'=4);
+	//[left] cte_est=0 & he_est=1 & pc=3 -> 1 : (a'=a_0_1) & (pc'=4);
+	//[right] cte_est=0 & he_est=2 & pc=3 -> 1 : (a'=a_0_2) & (pc'=4);
+	
+	//[straight] (cte_est=1 | cte_est=3) & he_est=0 & pc=3 -> 1 : (a'=a_13_0) & (pc'=4);
+	//[left] (cte_est=1 | cte_est=3) & he_est=1 & pc=3 -> 1 : (a'=a_13_1) & (pc'=4);
+	//[right] (cte_est=1 | cte_est=3) & he_est=2 & pc=3 -> 1 : (a'=a_13_2) & (pc'=4);
+	
+	//[straight] (cte_est=2 | cte_est=4) & he_est=0 & pc=3 -> 1 : (a'=a_24_0) & (pc'=4);
+	//[left] (cte_est=2 | cte_est=4) & he_est=1 & pc=3 -> 1 : (a'=a_24_1) & (pc'=4);
+	//[right] (cte_est=2 | cte_est=4) & he_est=2 & pc=3 -> 1 : (a'=a_24_2) & (pc'=4);
+
+	// airplane and environment: 
+	[] he=1 & a=1 & pc=4 & k<N -> 1 : (he'=-1) & (pc'=5); // error
+	[] he=2 & a=2 & pc=4 & k<N -> 1 : (he'=-1) & (pc'=5); // error
+
+	[] cte=0 & he=0 & a=0 & pc=4 & k<N -> 1 : (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=0 & he=0 & a=1 & pc=4 & k<N -> 1 : (cte'=1) & (he'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=0 & he=0 & a=2 & pc=4 & k<N -> 1 : (cte'=2) & (he'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=0 & he=1 & a=0 & pc=4 & k<N -> 1 : (cte'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=0 & he=1 & a=2 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=0 & he=2 & a=0 & pc=4 & k<N -> 1 : (cte'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=0 & he=2 & a=1 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	// left-side dynamics:
+	[] cte=1 & he=0 & a=0 & pc=4 & k<N -> 1 : (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=1 & he=0 & a=1 & pc=4 & k<N -> 1 : (cte'=3) & (he'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=1 & he=0 & a=2 & pc=4 & k<N -> 1 : (cte'=0) & (he'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=1 & he=1 & a=0 & pc=4 & k<N -> 1 : (cte'=3) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=1 & he=1 & a=2 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=1 & he=2 & a=0 & pc=4 & k<N -> 1 : (cte'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=1 & he=2 & a=1 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=3 & he=0 & a=0 & pc=4 & k<N -> 1 : (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=3 & he=0 & a=1 & pc=4 & k<N -> 1 : (cte'=-1) & (pc'=5); //error
+	[] cte=3 & he=0 & a=2 & pc=4 & k<N -> 1 : (cte'=1) & (he'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=3 & he=1 & a=0 & pc=4 & k<N -> 1 : (cte'=-1) & (pc'=5); //error
+	[] cte=3 & he=1 & a=2 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=3 & he=2 & a=0 & pc=4 & k<N -> 1 : (cte'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=3 & he=2 & a=1 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	// right-side dynamics:
+	[] cte=2 & he=0 & a=0 & pc=4 & k<N -> 1 : (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=2 & he=0 & a=1 & pc=4 & k<N -> 1 : (cte'=0) & (he'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=2 & he=0 & a=2 & pc=4 & k<N -> 1 : (cte'=4) & (he'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=2 & he=1 & a=0 & pc=4 & k<N -> 1 : (cte'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=2 & he=1 & a=2 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=2 & he=2 & a=0 & pc=4 & k<N -> 1 : (cte'=4) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=2 & he=2 & a=1 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=4 & he=0 & a=0 & pc=4 & k<N -> 1 : (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=4 & he=0 & a=1 & pc=4 & k<N -> 1 : (cte'=2) & (he'=1) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=4 & he=0 & a=2 & pc=4 & k<N -> 1 : (cte'=-1) & (pc'=5); //error
+
+	[] cte=4 & he=1 & a=0 & pc=4 & k<N -> 1 : (cte'=2) & (pc'=0) & (k'=k+1) & (a'=-1);
+	[] cte=4 & he=1 & a=2 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+	[] cte=4 & he=2 & a=0 & pc=4 & k<N -> 1 : (cte'=-1) & (pc'=5); //error
+	[] cte=4 & he=2 & a=1 & pc=4 & k<N -> 1 : (he'=0) & (pc'=0) & (k'=k+1) & (a'=-1);
+
+endmodule
+
+module crashed
+	crash: [0..1] init 0;
+	[] crash=0 & pc=5 -> 1:(crash'=1);
+endmodule
+
---
+++ tempest/test.py
@@ -0,0 +1,23 @@
+import pycarl
+
+from utils import *
+
+prism_path='taxinet_Boeing5bins_perfect_shielding.pm'
+prism_file='../system_models/taxinet_Boeing5bins_perfect_shielding.pm'
+formula_0='Pmax=? [G crash=0]'
+formula_1='Pmax=? [G he>-1]'
+storm_model=TaxiNetShieldHandler(prism_path, formula_0, shield_value=0.0, prism_file=prism_file)
+storm_model.create_shield()
+
+act_dict=storm_model.action_dictionary
+
+i=0
+max_i=10
+
+for key in act_dict:
+	print(key, act_dict[key])
+# 	i+=1
+# 	if i>max_i:
+# 		break
+
+# print(storm_model.action_dictionary)
